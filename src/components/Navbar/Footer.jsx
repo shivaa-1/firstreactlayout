21161c785f9a8395a74de413d8fd5a4cfbda22db
unknown <sharma.shivam2110@gmail.com>
2023-06-06 14:14:08 +0530

    added toggler button

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Footer=()=>{
     return(  
-    <div className="footer bg-dark m-2 p-3">
+    <div className="footer bg-dark m-2 p-2">
         <div className="footer-header text-center text-light">
             <h1 className="heading"><i class="bi bi-film"></i>  My Movies App<span className="text-secondary"> &copy;</span><sub className="text-secondary">copyright from abc pvt ltd.</sub></h1>
         </div>
-        <div className="body d-flex justify-content-around">
-            <ul className="left-part-links m-4">
+        <div className="body d-flex justify-content-between">
+            <ul className="left-part-links">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">Contact</a></li>
                 <li><a href="/">About Us</a></li>
                 <li><a href="/">Phone No. : 91-218380480</a></li>
             </ul>
-            <ul className="rigth-part-links m-4">
+            <ul className="rigth-part-links">
                 <li><a href="/">Follow Us</a></li>
                 <li><a href="/">Learn More</a></li>
                 <li><a href="/">Know More</a></li>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,17 @@ const Navbar=(prop)=>{
         <>
         <nav className="navbar bg-dark navbar-expand-lg p-2">
         <div className="container-fluid m-2 justify-content-around mx-5">
-            <a className="navbar-brand text-light" href="/"><i class="bi bi-film"></i>  My Movies</a>
+            <a className="navbar-brand text-light" href="/"><i className="bi bi-film"></i>  My Movies</a>
+            <button className="navbar-toggler border-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"><i className="bi bi-three-dots-vertical fs-1 text-info"></i></span>
+            </button>  
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
+                
                 <a className="nav-link text-light" href="/">About</a>
               </li>
             </ul>
